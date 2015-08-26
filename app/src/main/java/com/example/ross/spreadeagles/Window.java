0d9d2ac3a340ba5a6f70a6d8fd84c789cd3f7d8b
Steven Hanus <ministeve412@gmail.com>
2015-08-25 22:37:55 -0400

    new class that implements breakable

---
+++ app/src/main/java/com/example/ross/spreadeagles/Window.java
@@ -0,0 +1,65 @@
+package com.example.ross.spreadeagles;
+
+import android.util.Log;
+
+import org.andengine.entity.IEntity;
+import org.andengine.entity.modifier.MoveXModifier;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.util.modifier.IModifier;
+
+/**
+ * Created by Steve on 8/25/2015.
+ */
+public class Window extends Breakable {
+
+    public Window(float pX, float pY, ITextureRegion pTextureRegion, VertexBufferObjectManager pVertexBufferObjectManager, SpreadEaglesActivity pParent) {
+        super(pX, pY, pTextureRegion, pVertexBufferObjectManager, pParent);
+        this.setAlpha(1);
+    }
+
+    @Override
+    public void useBreakable(float x, float y, float width, float height) {
+        this.setInUse(true);
+        killed = false;
+        setPosition(x, y);
+        setHeight(height);
+        setWidth(width);
+        this.setVisible(true);
+        parentActivity.attachEntityWithZ(this, parentActivity.BREAKABLE_Z_DEPTH);
+        Log.v("Br-Used", "Window address" + address);
+    }
+
+    @Override
+    public void hit() {
+        hit = true;
+        parentActivity.addHit();
+        this.setAlpha(0);
+    }
+
+    @Override
+    public void recycleMe() {
+        Log.v("Break-Recycle", "Called");
+        this.setPosition(SpreadEaglesActivity.CAMERA_WIDTH / 2, SpreadEaglesActivity.CAMERA_HEIGHT);
+        this.setVisible(false);
+        this.setAlpha(1);
+        hit = false;
+        Log.v("Break-Recycle", "detached:" + this.detachSelf() + " and parent:" + this.getParent());
+        this.setInUse(false);
+    }
+
+    @Override
+    public void setMovements(MoveXModifier pIEntityModifier) {
+        pIEntityModifier.addModifierListener(new IModifier.IModifierListener<IEntity>() {
+            @Override
+            public void onModifierStarted(IModifier<IEntity> iEntityIModifier, IEntity iEntity) {
+            }
+
+            @Override
+            public void onModifierFinished(IModifier<IEntity> iEntityIModifier, IEntity iEntity) {
+                killMe();
+            }
+        });
+        this.registerEntityModifier(pIEntityModifier);
+    }
+}
