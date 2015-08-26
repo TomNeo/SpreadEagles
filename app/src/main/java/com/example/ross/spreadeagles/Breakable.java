995ae120e0c5433a804d189bd3a218209cbaabb3
Steven Hanus <ministeve412@gmail.com>
2015-08-25 22:38:28 -0400

    abstracted to allow new breakable item classes implement this breakable class

--- app/src/main/java/com/example/ross/spreadeagles/Breakable.java
+++ app/src/main/java/com/example/ross/spreadeagles/Breakable.java
@@ -11,44 +11,22 @@ import org.andengine.util.modifier.IModifier;
 /**
  * Created by Ross on 5/30/2015.
  */
-public class Breakable extends HeinousEntity{
+public abstract class Breakable extends HeinousEntity {
 
-    private SpreadEaglesActivity parentActivity;
-    private boolean hit;
-    private boolean killed;
+    public abstract void recycleMe();
+    public abstract void useBreakable(float x, float y, float width, float height);
+    public abstract void hit();
+    public abstract void setMovements(MoveXModifier pIEntityModifier);
+
+    public SpreadEaglesActivity parentActivity;
+    public boolean hit;
+    public boolean killed;
 
     public Breakable(float pX, float pY, ITextureRegion pTextureRegion, VertexBufferObjectManager pVertexBufferObjectManager, SpreadEaglesActivity pParent) {
         super(pX, pY, pTextureRegion, pVertexBufferObjectManager);
-        this.parentActivity  = pParent;
-//        this.setColor(1, 0, 0);
-        this.setAlpha(1);
+        this.parentActivity = pParent;
         hit = false;
         killed = false;
-    }
-
-    public void useBreakable(float x, float y, float width, float height){
-        this.setInUse(true);
-        killed = false;
-        setPosition(x, y);
-        setHeight(height);
-        setWidth(width);
-        this.setVisible(true);
-        parentActivity.attachEntityWithZ(this, parentActivity.BREAKABLE_Z_DEPTH);
-        Log.v("Br-Used", "Breakable address" + address);
-    }
-
-    public void setMovements(MoveXModifier pIEntityModifier){
-        pIEntityModifier.addModifierListener(new IModifier.IModifierListener<IEntity>() {
-            @Override
-            public void onModifierStarted(IModifier<IEntity> iEntityIModifier, IEntity iEntity) {
-            }
-
-            @Override
-            public void onModifierFinished(IModifier<IEntity> iEntityIModifier, IEntity iEntity) {
-                killMe();
-            }
-        });
-        this.registerEntityModifier(pIEntityModifier);
     }
 
     public void killMe(){
@@ -60,25 +38,6 @@ public class Breakable extends HeinousEntity{
 
     public boolean isHit(){
         return hit;
-    }
-
-    public void hit(){
-        hit = true;
-        parentActivity.addHit();
-//        this.setColor(1, 0, 0);
-        this.setAlpha(0);
-    }
-
-    @Override
-    public void recycleMe() {
-        Log.v("Break-Recycle", "Called");
-        this.setPosition(SpreadEaglesActivity.CAMERA_WIDTH / 2, SpreadEaglesActivity.CAMERA_HEIGHT);
-        this.setVisible(false);
-//        this.setColor(1, 0, 0);
-        this.setAlpha(1);
-        hit = false;
-        Log.v("Break-Recycle", "detached:" + this.detachSelf() + " and parent:" + this.getParent());
-        this.setInUse(false);
     }
 
 }
